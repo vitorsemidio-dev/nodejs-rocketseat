a70ee753a7ac5942738ed9e63df1d5dedf0b9a6c
Vitor Emidio <vitorsemidio@gmail.com>
2019-07-06 13:00:37 -0300

    Reestruturacao do projeto

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
@@ -11,16 +11,10 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
 
 
 requireDir('./src/models');
-const Product = mongoose.model('Product');
 
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'Bleach',
-        description: 'Bleach follows the adventures of Ichigo Kurosaki after he obtains the powers of a Soul Reaper',
-        url: 'https://en.wikipedia.org/wiki/Bleach_(TV_series)',
-    });
 
-    return res.send('Hello mongoose');
-});
+// Rotas
+app.use('/api', require('./src/routes'));
+
 
 app.listen(3000);
