d14132259670e816e36cf6083ce7e08700577cac
Vitor Emidio <vitorsemidio@gmail.com>
2019-07-06 13:37:38 -0300

    CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -11,9 +11,33 @@ module.exports = {
         return res.json(products);
     },
 
-    async store(req, res) {
-        const product = await Product.create(req.body);
+    async show(req, res) {
+        const { id } = req.params;
+        const product = await Product.findById(id);
 
         return res.json(product);
-    }
+    },
+
+    async store(req, res) {
+        const { body } = req;
+        const product = await Product.create(body);
+
+        return res.json(product);
+    },
+
+    async update(req, res) {
+        const { params, body } = req;
+        const { id } = params;
+        const returnProductUpdated = { new: true };
+        const product = await Product.findByIdAndUpdate(id, body, returnProductUpdated);
+
+        return res.json(product);
+    },
+
+    async destroy(req, res) {
+        const { id } = req.params;
+        await Product.findByIdAndRemove(id);
+
+        return res.send();
+    },
 }
