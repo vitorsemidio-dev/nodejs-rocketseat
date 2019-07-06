60cfbe741bc56be61b32d50397c2b110e3395a69
Vitor Emidio <vitorsemidio@gmail.com>
2019-07-06 12:15:22 -0300

    Configuracao para conexao com BD

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose');
 
+// Iniciando o App
 const app = express();
 
+
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+
 app.get('/', (req, res) => {
-    res.send('Hello Nodemon');
+    res.send('Hello mongoose');
 })
 
 app.listen(3000);
